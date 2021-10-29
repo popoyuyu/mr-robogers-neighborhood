b506751b7ffb6a9b3e0a57b1aa01471cb9c86097
Jeff Lai <pjefflai@gmail.com>
2021-10-29 10:01:07 -0700

    WIP: reading back on lessons to make first test work

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ function beepBoop(number) {
   const numArray = [];
   for (let i = 0; i <= number; i++) {
     if (number = 0) {
-      return true
+      return true;
     }
   }
 }
-console.log(beepBoop(0))
+console.log(beepBoop[0])
