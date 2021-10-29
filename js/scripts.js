4203c93691089899581e3e30ed7c53b97da34122
Jeff Lai <pjefflai@gmail.com>
2021-10-29 13:06:54 -0700

    WIP: lunch break

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ function beepBoop(number) {
   const numThree = "Wont you be my neighbor?";
   const numTwo = "Boop!"
   const numOne = "Beep!"
+  // split the inputted number into an array of numbers
+  // loop through the array of numbers and check if that number is either 1,2,3
+  // if it does = that end the function and return the number
   for (let i = 0; i <= number; i++) {
     if (i === 3) {
       numArray.push(numThree)
@@ -21,3 +24,5 @@ function beepBoop(number) {
   console.log(numArray);
 }
 console.log(beepBoop(5));
+
+lunch break
