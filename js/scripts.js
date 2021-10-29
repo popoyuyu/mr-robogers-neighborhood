4a5231a933028495cc90e74937b2202aa18eee08
Jeff Lai <pjefflai@gmail.com>
2021-10-29 13:55:50 -0700

    WIP: trying out .includes method

--- js/scripts.js
+++ js/scripts.js
@@ -6,23 +6,26 @@ function beepBoop(number) {
   // split the inputted number into an array of numbers
   // loop through the array of numbers and check if that number is either 1,2,3
   // if it does = that end the function and return the number
+
   for (let i = 0; i <= number; i++) {
-    if (i === 3) {
-      numArray.push(numThree)
+    if (i.toString().includes("3")) {
+      numArray.push("Won't you be my neighbor?")
     }
-    else if (i === 2) {
-      numArray.push(numTwo)
-    }
-    else if (i === 1) {
-      numArray.push(numOne)
-    }
+    //   if (i === 3) {
+    //     numArray.push(numThree)
+    //   }
+    //   else if (i === 2) {
+    //     numArray.push(numTwo)
+    //   }
+    //   else if (i === 1) {
+    //     numArray.push(numOne)
+    //   }
     else {
       numArray.push(i)
     }
-
+    return numArray
   }
+  // }
   console.log(numArray);
 }
-console.log(beepBoop(5));
-
-lunch break
+console.log(beepBoop(11));
