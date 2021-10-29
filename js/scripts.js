fa6e90fa4ebc0ff056841028f2d1d92646cf9aee
Jeff Lai <pjefflai@gmail.com>
2021-10-29 14:17:40 -0700

    .toString.includes method works and refactored code to less line

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,21 @@
 function beepBoop(number) {
   const numArray = [];
-  const numThree = "Wont you be my neighbor?";
-  const numTwo = "Boop!"
-  const numOne = "Beep!"
-  // split the inputted number into an array of numbers
-  // loop through the array of numbers and check if that number is either 1,2,3
-  // if it does = that end the function and return the number
 
   for (let i = 0; i <= number; i++) {
     if (i.toString().includes("3")) {
       numArray.push("Won't you be my neighbor?")
+    } else if (i.toString().includes("2")) {
+      numArray.push("Boop!")
+    } else if (i.toString().includes("1")) {
+      numArray.push("Beep!")
+    } else {
+      numArray.push(i)
     }
-    //   if (i === 3) {
+  }
+  return numArray
+}
+console.log(beepBoop(50));
+  //   if (i === 3) {
     //     numArray.push(numThree)
     //   }
     //   else if (i === 2) {
@@ -19,13 +23,5 @@ function beepBoop(number) {
     //   }
     //   else if (i === 1) {
     //     numArray.push(numOne)
-    //   }
-    else {
-      numArray.push(i)
-    }
-    return numArray
-  }
-  // }
-  console.log(numArray);
-}
-console.log(beepBoop(11));
+    //   } 
+     // }
