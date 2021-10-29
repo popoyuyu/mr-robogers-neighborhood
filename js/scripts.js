f1a4df5f724b528f9fbe8c24473fd8f8fceae2bc
Jeff Lai <pjefflai@gmail.com>
2021-10-29 12:25:21 -0700

    Fix bug where number would display after string still

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 function beepBoop(number) {
   const numArray = [];
   const numThree = "Wont you be my neighbor?";
+  const numTwo = "Boop!"
   for (let i = 0; i <= number; i++) {
     if (i === 3) {
       numArray.push(numThree)
     }
+    else if (i === 2) {
+      numArray.push(numTwo)
+    }
+    // else {
     numArray.push(i)
+    // }
+
   }
   console.log(numArray);
 }
